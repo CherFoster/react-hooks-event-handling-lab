9905457a92201c03e914602c50d06e30577bdbee
Cherise Foster <cherise.foster@outlook.com>
2023-04-17 10:05:56 -0600

    completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function handleChange(){
+    console.log("Entering password...")
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input onChange={handleChange} type='password' name='password ' placeholder='Enter Password:' />
+        </div>
     )
 }
 
